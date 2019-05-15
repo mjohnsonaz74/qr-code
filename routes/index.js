c1b45f26943a403a57329a5a905915bf5011dd59
Michael D. Johnson <michael.d.johnson1@charter.com>
2019-05-15 09:00:40 -0600

    update

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var parser = require('ua-parser-js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  const ua = parser(req.headers['user-agent']);
+  console.log(JSON.stringify(ua, null, '  '));
   res.render('index', { title: 'Express' });
 });
 
